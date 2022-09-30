01107a7672d9c939619477e1b0333e46d5450beb
DatTNT <dattnt24@gmail.com>
2022-09-30 16:00:05 +0700

    DOMEvents

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -5,7 +5,9 @@ class MyComponent extends React.Component {
     name: "TienDat",
     age: "4",
   };
-
+  handleClickButton = () => {
+    alert("click me");
+  };
   render() {
     let name = "Tien Dat";
 
@@ -13,6 +15,11 @@ class MyComponent extends React.Component {
       <div className="abc">
         <div className="a">Hello guys, My name is {this.state.name}</div>
         <div className="b">My ages is {this.state.age}</div>
+        <div className="c">
+          <button onClick={() => this.handleClickButton()}>click me</button>
+          {/* trong thằng onClick muốn gọi hàm handleClickButton thì bắt buộc phải tạo thêm 
+          1 arrow function trong thằng handleClickButton để nó hoạt động  */}
+        </div>
       </div>
     );
   }
