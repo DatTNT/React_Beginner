348fa2abc5a96066be3d9f18c4e486687f1e964f
DatTNT <dattnt24@gmail.com>
2022-10-14 15:49:03 +0700

    CreateComponent

--- src/views/App.js
+++ src/views/App.js
@@ -1,26 +1,32 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import MyComponent from "./Example/MyComponent";
+// import MyComponent from "./Example/MyComponentState";
+import MyComponent from "../components/MyComponent"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyComponent />
-      </header>
+    <div>
+      hoidanit
+      <MyComponent />
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     {/* <MyComponent /> */}
+    //   </header>
+    // </div>
   );
 }
 
