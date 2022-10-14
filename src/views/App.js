9f5d0686e86c8baa1940b1c13c77a555b6a79fc3
DatTNT <dattnt24@gmail.com>
2022-10-14 16:20:40 +0700

    What's State

--- src/views/App.js
+++ src/views/App.js
@@ -1,33 +1,48 @@
 import logo from "./logo.svg";
 import "./App.scss";
-// import MyComponent from "./Example/MyComponentState";
+// import { useDispatch, useSlector } from 'react-redux';
+// import { increaseCounter, decreaseCounter } from ".redux/action/counterAction"
 import MyComponent from "../components/MyComponent"
+import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      hoidanit
-      <MyComponent />
-    </div>
+class App extends React.Component {
+  state = {
+    name: 'DavidTran',
+    address: 'Hoi Dan IT',
+    age: 26
+  };
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     {/* <MyComponent /> */}
-    //   </header>
-    // </div>
-  );
+
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} i'm from {this.state.address}
+      </div>
+
+    );
+  }
 }
+
+
+
+
+
+
+// const App = () => {
+//   const count = useSlector(state => state.counter.count);
+//   const dispatch = useDispatch(); 
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world với Eric &amp;Hỏi Dân IT
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch}>Increase</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
