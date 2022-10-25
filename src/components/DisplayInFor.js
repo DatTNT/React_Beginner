7e759caeb43da9f1a1552c7b7972e26892b40973
DatTNT <dattnt24@gmail.com>
2022-10-25 20:46:16 +0700

    StatelessVsStateFull

--- src/components/DisplayInFor.js
+++ src/components/DisplayInFor.js
@@ -2,76 +2,113 @@ import React from 'react';
 import './DisplayInFor.scss';
 import logo from '../views/logo.svg'
 
-class DisplayInFo extends React.Component {
+// stateless vs statefull
+// class DisplayInFo extends React.Component {
 
-    constructor(props) {
-        console.log(">>> call constructor: 1")
-        super(props);
-        this.state = {
-            isShowListUser: true
-        }
-    }
-    componentDidMount() {
-        console.log(">>> call me component did mount")
+//     // constructor(props) {
+//     //     console.log(">>> call constructor: 1")
+//     //     super(props);
+//     //     this.state = {
+//     //         isShowListUser: true
+//     //     }
+//     // }
+//     // componentDidMount() {
+//     //     console.log(">>> call me component did mount")
 
-        setTimeout(() => {
-            document.title = "TienDat"
-        }, 3000)
-    }
+//     //     setTimeout(() => {
+//     //         document.title = "TienDat"
+//     //     }, 3000)
+//     // }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log(">>> call me component did update", this.props, prevProps);
-        if (this.props.listUser !== prevState.listUser) {
-            if (this.props.listUser.length > 5) {
-                alert("Numer of users are 5")
-            }
-        }
-    }
+//     // componentDidUpdate(prevProps, prevState, snapshot) {
+//     //     console.log(">>> call me component did update", this.props, prevProps);
+//     //     if (this.props.listUser !== prevState.listUser) {
+//     //         if (this.props.listUser.length > 5) {
+//     //             alert("Numer of users are 5")
+//     //         }
+//     //     }
+//     // }
 
-    handleOnHideShow = () => {
-        // this.state.isShowListUser ? this.setState({ isShowListUser: false }) : this.setState({ isShowListUser: true }); Dat viet
-        this.setState({ isShowListUser: !this.state.isShowListUser });
+//     // handleOnHideShow = () => {
+//     //     // this.state.isShowListUser ? this.setState({ isShowListUser: false }) : this.setState({ isShowListUser: true }); Dat viet
+//     //     this.setState({ isShowListUser: !this.state.isShowListUser });
 
-    }
+//     // }
 
 
-    render() {
-        console.log("call me render")
-        //destructing array/object
-        const { listUser } = this.props;
-        // console.log(listUser);
-        return (
-            <div className='display-infor-container'>
-                <img src={logo} alt="" />
-                <div>
+//     render() {
+//         console.log("call me render")
+//         //destructing array/object
+//         const { listUser } = this.props;
+//         // console.log(listUser);
+//         return (
+//             <div className='display-infor-container'>
+//                 <img src={logo} alt="" />
+//                 {/* <div>
+//                     <span onClick={() => { this.handleOnHideShow() }}>
+
+//                         {this.state.isShowListUser === true ? "Hide list of a Users:" : "Hint list of a Users:"}
+//                     </span>
+//                 </div> */}
+//                 {/* //dau && laf có nghĩa luôn luôn đúng khi dùng phép so sánh */}
+//                 {true &&
+//                     <>
+//                         {listUser.map((user) => {
+//                             // console.log(user)
+//                             //"+" this is convert String to Number
+//                             return (
+//                                 <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
+//                                     <div>This name: {user.name}</div>
+//                                     <div>This age: {user.age} </div>
+//                                     <div>
+//                                         <button onClick={() => this.props.handleDeleteuser(user.id)}>Delete</button>
+//                                     </div>
+//                                     <hr />
+
+//                                 </div>
+
+//                             )
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+const DisplayInFo = (props) => {
+    const { listUser } = props;
+    return (
+        <div className='display-infor-container'>
+            <img src={logo} alt="" />
+            {/* <div>
                     <span onClick={() => { this.handleOnHideShow() }}>
 
                         {this.state.isShowListUser === true ? "Hide list of a Users:" : "Hint list of a Users:"}
                     </span>
-                </div>
-                {/* //dau && laf có nghĩa luôn luôn đúng khi dùng phép so sánh */}
-                {this.state.isShowListUser &&
-                    <>
-                        {listUser.map((user) => {
-                            // console.log(user)
-                            //"+" this is convert String to Number
-                            return (
-                                <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
-                                    <div>This name: {user.name}</div>
-                                    <div>This age: {user.age} </div>
-                                    <div>
-                                        <button onClick={() => this.props.handleDeleteuser(user.id)}>Delete</button>
-                                    </div>
-                                    <hr />
-
+                </div> */}
+            {/* //dau && laf có nghĩa luôn luôn đúng khi dùng phép so sánh */}
+            {true &&
+                <>
+                    {listUser.map((user) => {
+                        // console.log(user)
+                        //"+" this is convert String to Number
+                        return (
+                            <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
+                                <div>This name: {user.name}</div>
+                                <div>This age: {user.age} </div>
+                                <div>
+                                    <button onClick={() => props.handleDeleteuser(user.id)}>Delete</button>
                                 </div>
+                                <hr />
 
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        )
-    }
+                            </div>
+
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
+
 }
 export default DisplayInFo;
