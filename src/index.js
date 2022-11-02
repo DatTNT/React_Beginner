d5be6974e5247fe9fd3d71d52ae2add5d5f3ab25
DatTNT <dattnt24@gmail.com>
2022-11-02 13:45:02 +0700

    Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./views/App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/global.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="users" element={<User />} />
+        <Route path="admins" element={<Admin />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
